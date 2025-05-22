ccd54f7c903bffd5b597aa8c7c93ed0fb954147d
samiframadhan <me@samiframadhan.com>
2025-05-22 19:52:32 +0700

    Change hpp for ros2 version above humble

--- laser_scan_matcher/src/laser_scan_matcher.cpp
+++ laser_scan_matcher/src/laser_scan_matcher.cpp
@@ -37,8 +37,8 @@
 #include <laser_scan_matcher/laser_scan_matcher.h>
 
 #include <tf2/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
 
 #undef min
 #undef max
